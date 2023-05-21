125f96a1faabb7f8d17e34a04da5eba887895400
clerov <byteblazedev@gmail.com>
2023-05-21 23:34:09 +0530

    updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,13 @@
+// import logo from '../assets/images/logo.png'
 const Home = () => {
     return ( 
         <main className="home w-full min-h-[80vh]">
             <section className="w-full min-h-[80vh] backdrop-blur-md flex flex-col justify-center items-center">
                    <div className="">
-                    <h1 className="font-aquire text-white text-4xl">
+                    {/* <img src={logo} alt="Taprobana Logo" className='home-logo w-40 mx-auto' /> */}
+                    <h1 className="font-aquire text-white text-4xl mt-4">
                         Welcome to taprobana
+                        <span className='w-24 h-2 bg-gradient-to-tr from-red-500 to-blue-800 block'></span>
                     </h1>
                    </div>
             </section>
